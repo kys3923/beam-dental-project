7a7e9cd3cd6675336842725ae9af78ab643d06d4
kys3923 <kys3923@gmail.com>
2021-05-16 19:33:50 -0400

    embedded vimeo

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -2,7 +2,7 @@ const Intro = (props) => {
   return (
     <section>
       <h1>Dental benefits that reward you for brushing your teeth.</h1>
-      {/* video embed here */}
+      <iframe src="https://player.vimeo.com/video/327864855?app_id=122963" width="710" height="401" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="Beam: Smarter Dental Care"></iframe>
     </section>
   );
 }
