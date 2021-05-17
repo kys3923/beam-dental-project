e4ba2691d04b0c67585593a616e33c6adeb0583b
kys3923 <kys3923@gmail.com>
2021-05-16 20:31:43 -0400

    uninstalled material-ui

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,10 +1,12 @@
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
 const Header = (props) => {
   return (
     <header>
       <img src='/img/Logo.png' alt='header logo' />
       <ul>
         {/* click to hover */}
-        <li>Explore</li>
+        <li>Explore <ExpandMoreIcon /></li>
         <li>Company</li>
         <li>Resources</li>
         {/* click to change page */}
