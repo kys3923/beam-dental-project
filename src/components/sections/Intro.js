2841ccac03e5d8ddadaddd78e82f7ed08844bab5
kys3923 <kys3923@gmail.com>
2021-05-17 02:13:17 -0400

    styled mission.js 1440px

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -1,7 +1,14 @@
 const Intro = (props) => {
   return (
-    <section>
-      <h1>Dental benefits that reward you for brushing your teeth.</h1>
+    <section className="intro_container">
+      <div className="grad_background" style={{ backgroundImage: "url(/img/GradientBG.png)" }}>
+        {/* <svg viewBox="0 0 1440 357" preserveAspectRatio="none">
+          <path d="M0.00,49.98 C149.99,150.00 271.49,-49.98 500.00,49.98 L500.00,0.00 L0.00,0.00 Z"></path>
+        </svg> */}
+      </div>
+      <div className="intro_textbox">
+        <h1>Dental benefits that reward you for brushing your teeth.</h1>
+      </div>
       <iframe src="https://player.vimeo.com/video/327864855?app_id=122963" width="710" height="401" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="Beam: Smarter Dental Care"></iframe>
     </section>
   );
--- src/components/sections/Mission.js
+++ src/components/sections/Mission.js
@@ -1,12 +1,16 @@
 const Mission = (props) => {
   return (
-    <section>
-      <img src='/img/Brush Image.png' alt='brush' />
-      <h1>Our Mission & Vision</h1>
-      <div className="rect"></div>
-      <p>Beam Dental was founded on the idea that everyone deserves access to dental care, centering our mission around closing the gap of the 100 million Americans who don't. By blending innovative technology with traditional insurance policies, we've been able to bring incredible value to the employee benefits market—both disrupting insurance forever and delivering on our mission.</p>
-      <p>Beam’s vision is to deliver a fundamentally unique approach to coverage by incorporating dental hygiene behavior into policy pricing at renewal.</p>
-      <img src='/img/Vision overhead Glasses.png' alt='glasses' />
+    <section className="mission_container">
+      <div className="mission_background_1" style={{ backgroundImage: "url(/img/BrushImage.png)" }}></div>
+      {/* <img src='/img/Brush Image.png' alt='brush' /> */}
+      <div className="mission_textbox">
+        <h1>Our Mission & Vision</h1>
+        <div className="rect"></div>
+        <p>Beam Dental was founded on the idea that everyone deserves access to dental care, centering our mission around closing the gap of the 100 million Americans who don't. By blending innovative technology with traditional insurance policies, we've been able to bring incredible value to the employee benefits market—both disrupting insurance forever and delivering on our mission.</p>
+        <p>Beam’s vision is to deliver a fundamentally unique approach to coverage by incorporating dental hygiene behavior into policy pricing at renewal.</p>
+      </div>
+      <div className="mission_background_2" style={{ backgroundImage: "url(/img/VisionOverheadGlasses.png)" }}></div>
+      {/* <img src='/img/Vision overhead Glasses.png' alt='glasses' /> */}
     </section>
   );
 }
