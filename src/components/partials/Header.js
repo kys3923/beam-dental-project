f46043895a27ebd11a36324af25f1be70b3d0ca0
kys3923 <kys3923@gmail.com>
2021-05-18 01:38:40 -0400

    added hamburger menu

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,21 +1,34 @@
-import { MdKeyboardArrowDown } from 'react-icons/md'
+import React, { useState } from 'react'
+import { MdKeyboardArrowDown, MdMenu, MdClose } from 'react-icons/md'
 
 const Header = (props) => {
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+  const closeMenu = () => setClick(false)
+
   return (
     <header>
       <img src='/img/Logo.png' alt='header logo' />
       <nav>
-        <ul>
+        <ul className={click ? "nav_options active" : "nav_options"}>
           {/* click to pop menu */}
-          <li>Explore <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
-          <li>Company <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
-          <li>Resources <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
+          <li className="option" onClick={closeMenu}>Explore <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
+          <li className="option" onClick={closeMenu}>Company <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
+          <li className="option" onClick={closeMenu}>Resources <span id="down_arrow"><MdKeyboardArrowDown /></span></li>
           {/* click to change page */}
           <div className="header_buttons">
-            <div id="find_dent_wrap"><div className="find_dent">Find a dentist</div></div>
-            <div id="login_box">Login</div>
+            <div id="find_dent_wrap"><div className="find_dent"><p>Find a dentist</p></div></div>
+            <div id="login_box"><p>Login</p></div>
           </div>
+          {/* hamburger menu */}
         </ul>
+        <div className="hamburger_menu" onClick={handleClick}>
+          {click ? (
+            <MdClose className="menu_icon" />
+          ) : (
+            <MdMenu className="menu_icon" />
+          )}
+        </div>
       </nav>
 
     </header>
