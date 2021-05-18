6c6735c0556d5987daaf89cbcff70152019fa1d1
kys3923 <kys3923@gmail.com>
2021-05-18 02:27:05 -0400

    final commit

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -3,40 +3,42 @@ const Footer = (props) => {
     <footer>
       <div className="footer_container">
         <img src='/img/Footer Logo.png' alt='footer logo' />
-        <ul>
-          <h3>Explore</h3>
-          <li>I'm a Broker</li>
-          <li>I'm a Member</li>
-          <li>I'm a Employer</li>
-          <li>I'm a Provider</li>
-        </ul>
-        <ul>
-          <h3>Learn</h3>
-          <li>FAQs</li>
-          <li>Company Blog</li>
-          <li>Product Blog</li>
-        </ul>
-        <ul>
-          <h3>Company</h3>
-          <li>About</li>
-          <li>Careers</li>
-          <li>Terms</li>
-          <li>Privacy</li>
-        </ul>
-        <ul>
-          <h3>Resources</h3>
-          <li>Find a Dentist</li>
-          <li>Contact Us</li>
-          <li>Get a Quote</li>
-          <li>Paper Claims</li>
-        </ul>
-        <ul>
-          <h3>Connect</h3>
-          <li>LinkedIn</li>
-          <li>Twitter</li>
-          <li>Facebook</li>
-          <li>Instagram</li>
-        </ul>
+        <div className="footer_nav">
+          <ul>
+            <h3>Explore</h3>
+            <li>I'm a Broker</li>
+            <li>I'm a Member</li>
+            <li>I'm a Employer</li>
+            <li>I'm a Provider</li>
+          </ul>
+          <ul>
+            <h3>Learn</h3>
+            <li>FAQs</li>
+            <li>Company Blog</li>
+            <li>Product Blog</li>
+          </ul>
+          <ul>
+            <h3>Company</h3>
+            <li>About</li>
+            <li>Careers</li>
+            <li>Terms</li>
+            <li>Privacy</li>
+          </ul>
+          <ul>
+            <h3>Resources</h3>
+            <li>Find a Dentist</li>
+            <li>Contact Us</li>
+            <li>Get a Quote</li>
+            <li>Paper Claims</li>
+          </ul>
+          <ul>
+            <h3>Connect</h3>
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
